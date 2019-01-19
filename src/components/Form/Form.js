82ed68354274274fe7a76e7ef6891ff1a87d6158
Masoud MI <masoud92m@gmail.com>
2019-01-19 22:32:43 +0330

    added add task to store

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,19 +1,41 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import './Form.css'
+import * as actionType from '../../store/actions';
 
 class Form extends Component {
+    state = { inputNewTast: '' }
+
+    inputChangeHanler = event => {
+        this.setState({ inputNewTast: event.target.value });
+    }
+
+    addButtonHanlder = () => {
+        const value = this.state.inputNewTast.trim();
+        if (value.length > 0) {
+            this.props.onAddTask(value);
+        }
+    }
+
     render() {
         return (
             <div className='row form-box'>
                 <div className='col-md-12'>
                     <div className="card bg-light text-dark">
                         <div className="card-body">
-                        <h5 className="card-title">Add New Task</h5>
+                            <h5 className="card-title">Add New Task</h5>
                             <div className="input-group mb-3" >
-                                <input type="text" className="form-control" placeholder="New Task" />
+                                <input type="text"
+                                    className="form-control"
+                                    placeholder="New Task"
+                                    value={this.state.inputNewTast}
+                                    onChange={(event) => this.inputChangeHanler(event)} />
                                 <div className="input-group-append">
-                                    <button className="btn btn-success" type="submit">Add Task</button>
+                                    <button
+                                        className="btn btn-success"
+                                        type="submit"
+                                        onClick={this.addButtonHanlder}>Add Task</button>
                                 </div>
                             </div>
                         </div>
@@ -24,4 +46,10 @@ class Form extends Component {
     }
 }
 
-export default Form;
+const mapDispatchToProps = dispatch => {
+    return {
+        onAddTask: (task) => dispatch({ type: actionType.ADD_TASK, task: task })
+    }
+}
+
+export default connect(null, mapDispatchToProps)(Form);
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,17 +2,20 @@ import * as actionType from './actions';
 
 const initialState = {
     tasks: [
-        { id: 1, value: 'angular' , status: 'unchecked' },
-        { id: 2, value: 'react' , status: 'checked' },
-        { id: 3, value: 'redux' , status: 'checked' },
-        { id: 4, value: 'vue.js' , status: 'unchecked' }
+        { id: 1, value: 'angular', status: 'unchecked' },
+        { id: 2, value: 'react', status: 'checked' },
+        { id: 3, value: 'redux', status: 'checked' },
+        { id: 4, value: 'vue.js', status: 'unchecked' }
     ]
 }
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionType.ADD_TASK:
-            return {}
+            return {
+                ...state,
+                tasks: state.tasks.concat({ id: Math.random(), value: action.task, status: 'unchecked' })
+            }
         case actionType.DELETE_TASK:
             return {}
         case actionType.CHECK_TASK:
