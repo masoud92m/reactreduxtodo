c8b00421b621f81ea3278cae993412e73f5dbcbc
Masoud MI <masoud92m@gmail.com>
2019-01-20 00:30:39 +0330

    added github url

--- src/containers/Todo/Todo.js
+++ src/containers/Todo/Todo.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import Form from '../../components/Form/Form';
 import Tasks from '../../components/Tasks/Tasks';
@@ -11,6 +12,8 @@ class Todo extends Component {
                     <h2 className="font-weight-bold">ReactRedux Todo</h2>
                     <Form />
                     <Tasks />
+                    <hr />
+                    <a rel="noopener noreferrer" href="https://github.com/masoud92m/reactreduxtodo" target="_blank" className="text-muted"><FontAwesomeIcon icon="copyright" /> TIme to Hack</a>
                 </div>
             </div>
         );
